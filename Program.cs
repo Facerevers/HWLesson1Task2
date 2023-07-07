773985ed091cb2cd77ba69a57b20cadaf190c749
crazle <crazle@mail.ru>
2023-07-07 17:21:35 +0300

    Написали программу, выдающую максимальное число из двух

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите первое число и нажмите Enter");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число и нажмите Enter");
+int b = int.Parse(Console.ReadLine());
+if (a > b) Console.WriteLine ($"max = {a}");
+if (a < b) Console.WriteLine ($"max = {b}");
+if (a == b) Console.WriteLine ("Оба числа равны, решайте сами");
